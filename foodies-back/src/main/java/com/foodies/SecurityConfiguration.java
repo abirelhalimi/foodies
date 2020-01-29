550621b95c39fd0800bc0196d7c0332336171680
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2020-01-29 16:27:12 +0100

    Update - Security configuration

--- foodies-back/src/main/java/com/foodies/SecurityConfiguration.java
+++ foodies-back/src/main/java/com/foodies/SecurityConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -47,6 +50,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+
+        return source;
     }
 
 }
--- foodies-back/src/main/java/com/foodies/controllers/DataController.java
+++ foodies-back/src/main/java/com/foodies/controllers/DataController.java
@@ -1,5 +1,6 @@
 package com.foodies.controllers;
 
+import com.foodies.models.Recommendation;
 import com.foodies.models.Restaurant;
 import com.foodies.models.User;
 import com.foodies.services.crud.RestaurantCrudService;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 
 @RequestMapping(value = "/api/mockData")
@@ -62,6 +65,17 @@ public class DataController {
         restaurant.setTelephone("5553215");
         restaurant.setUsername("mcdonalds");
         restaurantCrudService.add(restaurant);
+
+//        //Adding recommendations
+//        Recommendation recommendation = new Recommendation();
+//        Date date = new Date();
+//
+//        //first recommendation
+//        recommendation.setImage("myplate.png");
+//        recommendation.setRating("very good");
+//        recommendation.setText("I enjoyed my experience and the food was top notch");
+//        recommendation.setUser(userCrudService.getById((long) 1));
+////        recommendation.setDate();
 
         return "Database filled";
     }
